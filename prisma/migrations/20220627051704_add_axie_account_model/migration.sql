0f766c0ead6b8d216716651d88cd7ef66978d9e6
kelancelo <33194805+kelancelo@users.noreply.github.com>
2022-06-29 23:18:18 +0800

    changed db from sqlite to postgresql

--- prisma/migrations/20220627051704_add_axie_account_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "AxieAccount" (
-    "roninAdd" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "scholarShare" INTEGER NOT NULL,
-    "managerShare" INTEGER NOT NULL,
-    "userId" TEXT NOT NULL,
-    CONSTRAINT "AxieAccount_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
